291e1786f5a9765bb44a29434f62ca6615295470
Grigory Baleevskiy <baleevskii@gmail.com>
2017-06-16 11:17:31 +1000

    Fix lints

--- test/bad-schema.js
+++ test/bad-schema.js
@@ -6,7 +6,9 @@ describe('ahm-config: bad-schema', () => {
 
   it('should return schema errors when normalise disabled', (done) => {
     const onError = (err) => {
-      assert.equal(err.message, 'Config error: /a Has special characters in it (/^[a-z0-9_-]+$/i are allowed). Value \'/a\' should be string.');
+      assert.equal(
+        err.message,
+        'Config error: /a Has special characters in it (/^[a-z0-9_-]+$/i are allowed). Value \'/a\' should be string.');
       assert.deepEqual(err.meta, [{ field: '/a',
         message: 'Has special characters in it (/^[a-z0-9_-]+$/i are allowed)',
         value: '/a',
